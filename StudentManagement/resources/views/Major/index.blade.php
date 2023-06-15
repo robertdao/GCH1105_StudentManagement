b549582814c6b6789990a6edc5549549b696eabe
robertdao <iamdvh2003@gmail.com>
2023-06-15 16:20:57 +0700

    Update index.blade.php

--- StudentManagement/resources/views/Major/index.blade.php
+++ StudentManagement/resources/views/Major/index.blade.php
@@ -1,27 +1,25 @@
 @extends('layout.layout')
-@section('tile','List Major')
+@section('title','List Major')
 @section('content')
 <div class="table-responsive">
-    <table class="table table-striped table-hover">
+    <table class="table-show">
         <thead class="table-light">
             <tr>
-                <th>Major Name</th>
-                <th>Major Subject</th>
+                <th class="head-table">Major Name</th>
+                <th class="head-table">Major Subject</th>
+                <th class="head-table">Action</th>
             </tr>
             </thead>
             <tbody class="table-group-divider">
                   @foreach ($majors as $major)
                   <tr class="table-primary" >
-                    <td>
+                    <td class="tr-content">
                       <a href="{{url("/major/".$major->id)}}">
                       {{$major->name}}
                       </a>
-                    </td>
-                    <td>{{$major->subject}}</td>
-                    <td>
-                      @foreach ($majors as $major)
-                        <label>{{$major->name}}</label>
-                      @endforeach
+                    </td class="tr-content">
+                    <td class="tr-content">{{$major->subject}}</td>
+                    <td class="tr-content">
                       <a href="{{url("/major/".$major->id)}}" class="btn btn-primary">View</a>
                       <a href="{{url("/major/".$major->id."/edit")}}" class="btn btn-warning">Edit</a>
                       <form action="{{url("/major/".$major->id)}}" method="post" class="d-inline">
