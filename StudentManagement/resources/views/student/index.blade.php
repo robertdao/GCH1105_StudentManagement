826a36879a66843a72ce9ca1e2b7eebdf9a4d453
hungbaoday <133212196+hungbaoday@users.noreply.github.com>
2023-06-17 17:22:43 +0700

    edit if else in index student

--- StudentManagement/resources/views/student/index.blade.php
+++ StudentManagement/resources/views/student/index.blade.php
@@ -4,56 +4,56 @@
 
 
 @if (count($students) > 0)
-    <h1>Dont have student</h1>
+    <div class="container-classroom">
+        <table class="table-show">
+                <tr class="tr-top">
+                    <th class="head-table">Student ID</th>
+                    <th class="head-table">Name</th>
+                    <th class="head-table">Birth Year</th>
+                    <th class="head-table">Email</th>
+                    <th class="head-table">Photo</th>
+                    <th class="head-table">Class</th>
+                    <th class="head-table">Major</th>
+                    <th class="head-table">Action</th>
+                </tr>
+                @foreach ($students as $student)
+                <tr class="tr-content">
+                    <td class="content-table">
+                        {{$student->student_id}}
+                    </td>
+                    <td class="content-table">
+                        {{$student->name}}
+                    </td>
+                    
+                    <td class="content-table">
+                        {{$student->birth}}
+                    </td>
+                    <td class="content-table">
+                        {{$student->email}}
+                    </td>
+                    <td class="content-table">
+                        <img class="img-student" src="{{url("/upload/".$student->photo)}}" alt="">
+                    </td>
+                    <td class="content-table">
+                        {{$student->classes->name}}
+                    </td>
+                    <td class="content-table">
+                        {{$student->majors->name}}
+                    </td>
+    
+                    <td>
+                        <a href="{{url("/students/".$student->id)}} ">View</a>
+                        <a href="{{url("/students/".$student->id."/edit")}}" class="btn btn-warning">Edit</a>
+                        <form action="{{"/students/".$student->id}}" method="post"class="d-inline" >
+                          {{method_field('Delete')}}
+                          @csrf
+                          <button type='submit' class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                    </td>
+                </tr>
+                @endforeach
+        </table>
+    </div>
 @else
-<div class="container-classroom">
-    <table class="table-show">
-            <tr class="tr-top">
-                <th class="head-table">Student ID</th>
-                <th class="head-table">Name</th>
-                <th class="head-table">Birth Year</th>
-                <th class="head-table">Email</th>
-                <th class="head-table">Photo</th>
-                <th class="head-table">Class</th>
-                <th class="head-table">Major</th>
-                <th class="head-table">Action</th>
-            </tr>
-            @foreach ($students as $student)
-            <tr class="tr-content">
-                <td class="content-table">
-                    {{$student->student_id}}
-                </td>
-                <td class="content-table">
-                    {{$student->name}}
-                </td>
-                
-                <td class="content-table">
-                    {{$student->birth}}
-                </td>
-                <td class="content-table">
-                    {{$student->email}}
-                </td>
-                <td class="content-table">
-                    <img class="img-student" src="{{url("/upload/".$student->photo)}}" alt="">
-                </td>
-                <td class="content-table">
-                    {{$student->classes->name}}
-                </td>
-                <td class="content-table">
-                    {{$student->majors->name}}
-                </td>
-
-                <td>
-                    <a href="{{url("/students/".$student->id)}} ">View</a>
-                    <a href="{{url("/students/".$student->id."/edit")}}" class="btn btn-warning">Edit</a>
-                    <form action="{{"/students/".$student->id}}" method="post"class="d-inline" >
-                      {{method_field('Delete')}}
-                      @csrf
-                      <button type='submit' class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                </td>
-            </tr>
-            @endforeach
-    </table>
-</div>
+<h1>Dont have student</h1>
 @endif
 @endsection
